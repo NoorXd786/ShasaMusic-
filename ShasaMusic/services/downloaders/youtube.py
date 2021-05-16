0b7e93e6cac3c1ca6d0e20b00072f15832097733
Mdnoor786 <75603153+Mdnoor786@users.noreply.github.com>
2021-05-16 13:39:47 +0530

    Add files via upload

--- ShasaMusic/services/downloaders/youtube.py
+++ ShasaMusic/services/downloaders/youtube.py
@@ -20,8 +20,8 @@ from os import path
 
 from youtube_dl import YoutubeDL
 
-from DaisyXMusic.config import DURATION_LIMIT
-from DaisyXMusic.helpers.errors import DurationLimitError
+from ShasaMusic.config import DURATION_LIMIT
+from ShasaMusic.helpers.errors import DurationLimitError
 
 ydl_opts = {
     "format": "bestaudio[ext=m4a]",
