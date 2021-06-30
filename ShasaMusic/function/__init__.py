40681a1c3cca1ac064eb26b1666ef329418c6e65
Mdnoor786 <75603153+Mdnoor786@users.noreply.github.com>
2021-06-30 21:49:27 +0530

    Add files via upload

--- ShasaMusic/function/__init__.py
+++ ShasaMusic/function/__init__.py
@@ -1,3 +1,5 @@
-from ShasaMusic.function.admins import admins, get, set
+from ShasaMusic.function.admins import admins
+from ShasaMusic.function.admins import get
+from ShasaMusic.function.admins import set
 
 __all__ = ["set", "get", "admins"]
