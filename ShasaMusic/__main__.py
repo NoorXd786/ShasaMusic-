39ae7118306f75cae279ba4e0def12eef9b5fbfc
Mdnoor786 <75603153+Mdnoor786@users.noreply.github.com>
2021-06-30 21:54:31 +0530

    Add files via upload

--- ShasaMusic/__main__.py
+++ ShasaMusic/__main__.py
@@ -1,5 +1,5 @@
-# Calls Music 1 - Telegram bot for streaming audio in group calls
-# Copyright (C) 2021  Roj Serbest
+# ShasaMusic (Telegram bot project)
+# Copyright (C) 2021  Inuka Asith & Rojserbest
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -18,7 +18,10 @@
 import requests
 from pyrogram import Client as Bot
 
-from ShasaMusic.config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
+from ShasaMusic.config import API_HASH
+from ShasaMusic.config import API_ID
+from ShasaMusic.config import BG_IMAGE
+from ShasaMusic.config import BOT_TOKEN
 from ShasaMusic.services.callsmusic import run
 
 response = requests.get(BG_IMAGE)
--- ShasaMusic/config.py
+++ ShasaMusic/config.py
@@ -16,8 +16,8 @@
 # Modified by Inukaasith
 
 import os
+from os import path
 from os import getenv
-
 from dotenv import load_dotenv
 
 if os.path.exists("local.env"):
@@ -27,20 +27,19 @@ que = {}
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_NAME = getenv("BOT_NAME")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "LionXupdates")
-BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/693fda7ab2f4be48856c8.jpg")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "Shasaupdates")
+BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/dcfdf612e499eef0e0b1f.png")
 admins = {}
-API_ID = int(getenv("API_ID"))
+API_ID = int(getenv("API_ID", ""))
 API_HASH = getenv("API_HASH")
 BOT_USERNAME = getenv("BOT_USERNAME")
 ASSISTANT_NAME = getenv("ASSISTANT_NAME", "Shasahelper")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "ShasaSupport")
-PROJECT_NAME = getenv("PROJECT_NAME", "ShasaMusic v4")
-SOURCE_CODE = getenv("SOURCE_CODE", "github.com/TeamLion-X/Shasamusic")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "ShasaSupport_Official")
+PROJECT_NAME = getenv("PROJECT_NAME", "ShasaMusic v5")
+SOURCE_CODE = getenv("SOURCE_CODE", "github.com/TeamShasa/ShasaMusic")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
 ARQ_API_KEY = getenv("ARQ_API_KEY", None)
 PMPERMIT = getenv("PMPERMIT", None)
 LOG_GRP = getenv("LOG_GRP", None)
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
-
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
