bce0c6f2c9991c6a293574f912a691da520e5dee
Mdnoor786 <75603153+Mdnoor786@users.noreply.github.com>
2021-07-19 07:55:45 +0530

    Add files via upload

--- ShasaMusic/services/queues/__init__.py
+++ ShasaMusic/services/queues/__init__.py
@@ -1,7 +1,3 @@
-from ShasaMusic.services.queues.queues import clear 
-from ShasaMusic.services.queues.queues import get
-from ShasaMusic.services.queues.queues import is_empty
-from ShasaMusic.services.queues.queues import put
-from ShasaMusic.services.queues.queues import task_done
+from ShasaMusic.services.queues.queues import clear, get, is_empty, put, task_done
 
 __all__ = ["clear", "get", "is_empty", "put", "task_done"]
