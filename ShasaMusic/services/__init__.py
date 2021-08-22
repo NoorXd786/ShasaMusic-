f56e671b00e359f4ef8a65dbbb9dcb697c7f15fb
Mdnoor786 <75603153+Mdnoor786@users.noreply.github.com>
2021-08-22 16:15:34 +0530

    Update __init__.py

--- ShasaMusic/services/__init__.py
+++ ShasaMusic/services/__init__.py
@@ -1,18 +1,1 @@
-from ShasaMusic.services.callsmusic import pytgcalls, run
-from ShasaMusic.services.queues import queues
 
-__all__ = ["queues", "pytgcalls", "run"]
-from os import listdir, mkdir
-
-if "raw_files" not in listdir():
-    mkdir("raw_files")
-
-from ShasaMusic.services.converter import convert
-
-__all__ = ["convert"]
-from ShasaMusic.services import youtube
-
-__all__ = ["youtube"]
-from ShasaMusic.services.queues import clear, get, is_empty, put, task_done
-
-__all__ = ["clear", "get", "is_empty", "put", "task_done"]
